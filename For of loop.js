ed442539544e2da9e84e51a9eede615660fed180
Jasir Hassan K <jasirhassan.894315@gmail.com>
2026-02-12 21:43:43 +0530

    Add examples of for-of loop usage in JavaScript

---
+++ For of loop.js
@@ -0,0 +1,29 @@
+var arr1 = ["apple","orange","banana"];
+var num = [10,20,30];
+//for of loop
+
+for(let x of arr1) {
+    console.log(x )
+}
+
+for(let x of arr1) {
+    console.log(x + "is veg")
+}
+
+for(let x of num) {
+    console.log(x *10)
+}
+
+for(let x of num) {
+    console.log(x )
+}
+
+var str = "Hello";
+for(let x of str) {
+    console.log(x);
+}
+
+var str = "Hello";
+for(let x of str) {
+    console.log(x+"ing");
+}
