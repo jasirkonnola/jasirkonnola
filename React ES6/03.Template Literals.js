6a2ffd345847bb976610a8541f871c10bf6bd7af
Jasir Hassan K <jasirhassan.894315@gmail.com>
2026-02-13 11:27:54 +0530

    Refactor console logs to use template literals

---
+++ React ES6/03.Template Literals.js
@@ -0,0 +1,24 @@
+var name = "jasir";
+var age = 25;
+
+
+console.log("name is "+name+" im "+age+" year old")
+
+
+// Template literal
+console.log(`My name is ${name} im ${age} years old `);
+
+
+/var a = 10;
+var b = 20;
+
+/console.log(`sum of a and b is ${a+b}`);
+
+console.log("My name is jasir");
+console.log("i m 25 year old");
+
+
+// multiline
+
+console.log(`My name is jasir,
+im 20 years old`);
